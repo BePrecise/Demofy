31a6c9f6220a9b7fcabef5ddff9e57412fdadc81
Harm Haandrikman <hfhaandrikman@gmail.com>
2024-03-19 13:18:53 +0100

    added a startup file, now it takes dotenv variables for security etc

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,20 @@
-require `dotenv/load`
+require 'dotenv/load'
+
+def main
+    client_id = ENV['SPOTIFY_CLIENT_ID']
+    client_secret = ENV['SPOTIFY_CLIENT_SECRET']
+    startup(client_id, client_secret)
+
+end
+
+def startup(client_id, client_secret)
+    puts "Starting up..."
+    if client_id.nil? || client_secret.nil?
+        puts "Please set SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET in .env"
+        exit
+    end
+    puts "Client ID: #{client_id}"
+    puts "Client Secret: #{client_secret}"
+    end
+
+main
